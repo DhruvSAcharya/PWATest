c63d0e5e8e9984ce642b26e59a8bd85f1a58f160
Dhruv Acharya <dhruv.acharya@synoverge.com>
2022-03-12 10:23:06 +0530

    Add project files.

---
+++ PWATest/wwwroot/ServiceWorker.js
@@ -0,0 +1,41 @@
+﻿const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/lib/bootstrap/dist/css/bootstrap.min.css',
+    '/css/site.css',
+    '/lib/jquery/dist/jquery.min.js',
+    '/lib/bootstrap/dist/js/bootstrap.bundle.min.js',
+    '/js/site.js?v=4q1jwFhaPaZgr8WAUSrux6hAuh0XDg9kPS3xIVq36I0',
+    '/js/app.js',
+    '/ServiceWorker.js',
+    '/manifest.json',
+    '/favicon.ico',
+    '/images/windows11/Square150x150Logo.scale-150.png'
+];
+
+self.addEventListener('install', evt => {
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log("data is caching");
+            cache.addAll(assets);
+        })
+    );
+    //console.log('service worker has been installed')
+    
+});
+
+//change abc
+self.addEventListener('activate', evt => {
+    //console.log('service worker has been activated')
+});
+
+
+self.addEventListener('fetch', evt => {
+    console.log(evt.request.method + " " + evt.request.url)
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    );
+
+});
---
+++ PWATest/wwwroot/js/app.js
@@ -0,0 +1,6 @@
+﻿if ('serviceWorker' in navigator) {
+    console.log("service worker is supported");
+    navigator.serviceWorker.register('../ServiceWorker.js')
+        .then((reg) => console.log(reg))
+        .catch((err) => console.log('not register: '+err));
+}
